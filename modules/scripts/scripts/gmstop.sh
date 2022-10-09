050fd3a9df8810811f13cbdd49dee397f4cf991b
quasigod-io <quasigod-io@protonmail.com>
2022-10-09 01:55:59 -0400

    Deleted files no longer needed due to 627b15

--- modules/scripts/scripts/gmstop.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env sh
-
-echo 'madvise' | sudo tee /sys/kernel/mm/transparent_hugepage/enabled
-# picom --experimental-backends --unredir-if-possible
-~/.scripts/togdnd.sh -u
-polybar-msg action gamemode module_hide
