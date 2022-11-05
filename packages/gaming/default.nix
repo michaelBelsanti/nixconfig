ab71642ca8c3d6783d60c7f056cb16555997ef0d
quasigod-io <quasigod-io@protonmail.com>
2022-11-04 23:21:44 -0400

    Gaming package refactoring

--- packages/gaming/default.nix
+++ packages/gaming/default.nix
@@ -1,11 +1,20 @@
 { config, pkgs, lib, inputs, ... }:
 let 
   wine-tkg = inputs.nix-gaming.packages.${pkgs.system}.wine-tkg;
+  gmstart = (pkgs.writeShellScriptBin "gmstart" ''
+      echo 'always' | sudo tee /sys/kernel/mm/transparent_hugepage/enabled
+      polybar-msg action gamemode module_show
+      togdnd -p
+    '');
+  gmstop = (pkgs.writeShellScriptBin "gmstop" ''
+      echo 'madvise' | sudo tee /sys/kernel/mm/transparent_hugepage/enabled
+      polybar-msg action gamemode module_hide
+      togdnd -u
+    '');
 in
 {
   # Using mkForce because lib.mkDefault can't be used in nixos/configuration.nix
   boot.kernelPackages = lib.mkForce pkgs.linuxPackages_xanmod_latest;
-  programs.gamemode.enable = true;
   environment.systemPackages = with pkgs; [
     gamescope
     winetricks
@@ -19,19 +28,45 @@ in
     prismlauncher
     
     # For use with gamemode. Hugepages can increase performance in some games
-    (writeShellScriptBin "gmstart" ''
-      echo 'always' | sudo tee /sys/kernel/mm/transparent_hugepage/enabled
-      polybar-msg action gamemode module_show
-      togdnd -p
-    '')
-    (writeShellScriptBin "gmstop" ''
-      echo 'madvise' | sudo tee /sys/kernel/mm/transparent_hugepage/enabled
-      polybar-msg action gamemode module_hide
-      togdnd -u
-    '')
+    # (writeShellScriptBin "gmstart" ''
+    #   echo 'always' | sudo tee /sys/kernel/mm/transparent_hugepage/enabled
+    #   polybar-msg action gamemode module_show
+    #   togdnd -p
+    # '')
+    # (writeShellScriptBin "gmstop" ''
+    #   echo 'madvise' | sudo tee /sys/kernel/mm/transparent_hugepage/enabled
+    #   polybar-msg action gamemode module_hide
+    #   togdnd -u
+    # '')
   ];
 
-  programs.steam.enable = true;
-  programs.steam.remotePlay.openFirewall = true;
   hardware.steam-hardware.enable = true;
+  programs = {
+    steam = {
+      enable = true;
+      remotePlay.openFirewall = true;
+    };
+    gamemode = {
+      enable = true;
+      settings = {
+        general = {
+          reaper_freq = 5;
+          defaultgov = "performance";
+          softrealtime = "auto";
+          renice = 0;
+          ioprio = 0;          
+        };
+        gpu = {
+          apply_gpu_optimisations = "accept-responsibility";
+          gpu_device = 0;
+          nv_powermizer_mode = 1;
+          amd_performance_level = "high";
+        };
+        custom = {
+          start = "${gmstart}/bin/gmstart";
+          end = "${gmstop}/bin/gmstop";
+        };
+      };
+    };
+  };
 }
