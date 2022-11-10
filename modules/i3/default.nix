1a7d3f34cef8eb296fe19e021b07374473bc1da7
quasigod-io <quasigod-io@protonmail.com>
2022-11-10 13:13:29 -0500

    Switched picom versions

--- modules/i3/default.nix
+++ modules/i3/default.nix
@@ -3,7 +3,8 @@
   config = lib.mkIf config.services.xserver.windowManager.i3.enable {
     services.xserver.windowManager.i3.package = pkgs.i3-gaps;
     environment.systemPackages = with pkgs; [
-      picom-jonaburg
+      # picom-jonaburg
+      picom # jonaburg seems to be causing performance issues in game, maybe an issue with unredir-if-possible?
       wezterm
       alacritty
       betterlockscreen
