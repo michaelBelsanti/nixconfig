f43ff87aaddac319dc6c3d9b88e67b8ff3558a19
quasigod-io <quasigod-io@protonmail.com>
2022-12-19 18:43:58 -0500

    Created loadEnv function to generate Nushell environment variables

--- modules/shell/nushell/default.nix
+++ modules/shell/nushell/default.nix
@@ -1,4 +1,10 @@
-{config, pkgs, lib, flakePath, ... }: {
+{config, pkgs, lib, flakePath, ... }: 
+let
+  loadEnvVars = n: v: ''"${n}": "${toString v}"'';
+
+  loadEnv = vars: ''load-env { ${lib.concatStringsSep ", " (lib.mapAttrsToList loadEnvVars vars)} }'';
+in
+{
   programs = {
     nushell = {
       enable = true;
@@ -12,6 +18,7 @@
         alias nixup = doas nixos-rebuild switch --flake '${flakePath}'
         alias nixUp = nix flake update ${flakePath} && doas nixos-rebuild switch --flake '${flakePath}'
       '';
+      extraEnv = ''${loadEnv config.home.sessionVariables}'';
     };
   };
 }
