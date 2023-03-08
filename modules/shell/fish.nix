c73b94e7c7f876d9643d2748b521e7fe2ceeeaf4
michaelBelsanti <quasigod-io@protonmail.com>
2023-03-08 13:34:31 -0500

    Updated fish config with plugins

--- modules/shell/fish.nix
+++ modules/shell/fish.nix
@@ -1,10 +1,13 @@
-{ config, pkgs, flakePath, ... }: {
+{ config, pkgs, flakePath, ... }:
+  let fishPlugins = pkgs.fishPlugins; in
+{ 
+  home.packages = with pkgs; [ libnotify ]; # For `done` plugin
   programs = {
     fish = {
       enable = true;
       shellAliases = {
         nixup = 
-          "doas nixos-rebuild switch --flake ${flakePath} && source ~/.config/zsh/.zshrc";
+          "doas nixos-rebuild switch --flake ${flakePath} && source ~/.config/fish/config.fish";
         ls = "exa -al";
         lt = "exa -aT";
         cat = "bat";
@@ -14,6 +17,14 @@
       shellAbbrs = {
         lg = "lazygit";
       };
+      plugins = [
+        { name = "pure"; src = fishPlugins.pure.src; }
+        { name = "done"; src = fishPlugins.done.src; }
+        { name = "sponge"; src = fishPlugins.sponge.src; }
+        { name = "pisces"; src = fishPlugins.pisces.src; }
+        { name = "fzf-fish"; src = fishPlugins.fzf-fish.src; }
+        { name = "colored-man-pages"; src = fishPlugins.colored-man-pages.src; }
+      ];
     };
   };
 }
