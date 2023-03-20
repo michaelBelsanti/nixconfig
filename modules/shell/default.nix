7b461bae8cb94df55b26b18150de5630eba675cc
michaelBelsanti <quasigod-io@protonmail.com>
2023-03-20 12:42:53 -0400

    Remove redundant settings

--- modules/shell/default.nix
+++ modules/shell/default.nix
@@ -2,10 +2,9 @@
   # Nushell requires more in-depth configuration
   imports = [ ./nushell ./fish.nix ./zsh.nix ./ion.nix];
   programs = {
-    starship = {
-      enable = true;
-      enableIonIntegration = true;
-    };
+    starship.enable = true;
+    zoxide.enable = true;
+    fzf.enable = true;
     direnv = {
       enable = true;
       nix-direnv.enable = true;
@@ -13,13 +12,6 @@
     exa = {
       enable = true;
       enableAliases = true;
-    };
-    zoxide.enable = true;
-    fzf = {
-      enable = true;
-      enableFishIntegration = true;
-      enableZshIntegration = true;
-      enableBashIntegration = true;
     };
     bash = {
       enable = true;
