3c38005adb95f84f6e6d37e2f0664a2bdc8d959a
michaelBelsanti <quasigod-io@protonmail.com>
2023-04-19 12:02:40 -0400

    Updated inputs

--- nixos/default.nix
+++ nixos/default.nix
@@ -16,7 +16,7 @@
       "transparent_hugepage" = "always"; "vm.nr_hugepages_defrag" = 0; "ipcs_shm" = 1;
       "default_hugepagez" = "1G"; "hugepagesz" = "1G"; "vm.swappiness" = 1; "vm.compact_memory" = 0;
     };
-    tmpOnTmpfs = true;
+    tmp.useTmpfs = true;
     # Cute boot animation
     plymouth.enable = true;
     loader = {
