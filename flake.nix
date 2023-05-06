f1e6e95267c46da0f6860318c7c3ee347483927e
michaelBelsanti <quasigod-io@protonmail.com>
2023-05-05 20:54:33 -0400

    Switched many inputs to following nixpkgs

--- flake.nix
+++ flake.nix
@@ -1,27 +1,5 @@
 {
   description = "Quasigod's NixOS config";
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    flake-parts.url = "github:hercules-ci/flake-parts";
-    hm = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    nur.url = "github:nix-community/nur";
-    mypkgs.url = "github:michaelBelsanti/nur-packages";
-
-    nixos-hardware.url = "github:nixos/nixos-hardware";
-    nix-gaming.url = "github:fufexan/nix-gaming";
-
-    plasma-manager.url = "github:pjones/plasma-manager";
-    hyprland.url = "github:hyprwm/Hyprland/v0.24.1";
-    hypr-contrib.url = "github:hyprwm/contrib";
-
-    helix.url = "github:helix-editor/helix";
-    devenv.url = "github:cachix/devenv/v0.4";
-    spicetify.url = "github:the-argus/spicetify-nix";
-    nix-alien.url = "github:thiagokokada/nix-alien";
-  };
   outputs = inputs@{flake-parts, ... }:
     let
       user = "quasi";
@@ -48,4 +26,46 @@
         };
       };
     };
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+    hm = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nur.url = "github:nix-community/nur";
+    mypkgs.url = "github:michaelBelsanti/nur-packages";
+
+    nixos-hardware.url = "github:nixos/nixos-hardware";
+    nix-gaming.url = "github:fufexan/nix-gaming";
+
+    plasma-manager.url = "github:pjones/plasma-manager";
+
+    hyprland.url = "github:hyprwm/Hyprland/v0.24.1";
+    hypr-contrib = {
+      url = "github:hyprwm/contrib";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    helix.url = "github:helix-editor/helix";
+
+    devenv.url = "github:cachix/devenv/v0.4";
+
+    spicetify = {
+      url = "github:the-argus/spicetify-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    nix-alien = {
+      url = "github:thiagokokada/nix-alien";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nix-index-db = {
+      url = "github:Mic92/nix-index-database";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
 }
