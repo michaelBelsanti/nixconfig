72e7a5e37bec8a37916e9697a61530fbb5531f3f
michaelBelsanti <quasigod-io@protonmail.com>
2023-07-18 21:14:47 -0400

    Updated nushell config

--- home/modules/shell/nushell/default.nix
+++ home/modules/shell/nushell/default.nix
@@ -3,30 +3,34 @@
   flakePath,
   ...
 }: let
-  nuprompt = pkgs.writeText "nuprompt" "${builtins.readFile ./panache-git.nu}";
+  # nuprompt = pkgs.writeText "nuprompt" "${builtins.readFile ./panache-git.nu}";
 in {
   home.packages = with pkgs; [carapace];
   programs = {
+    starship.enableNushellIntegration = true;
     nushell = {
       enable = true;
-      envFile.source = ./env.nu;
-      configFile.source = ./config.nu;
+      # envFile.source = ./env.nu;
+      # configFile.source = ./config.nu;
       extraConfig = ''
-        nitch
+        # nitch
 
         alias ls = ls -a
         alias lg = lazygit
         alias o = xdg-open
-        alias nixup = doas nixos-rebuild switch --flake '${flakePath}'
-        alias nixUp = (nix flake update ${flakePath}; doas nixos-rebuild switch --flake '${flakePath}')
+        # alias nixup = doas nixos-rebuild switch --flake '${flakePath}'
+        # alias nixUp = (nix flake update ${flakePath}; doas nixos-rebuild switch --flake '${flakePath}')
+
+        # carapace setup
+        source ~/.cache/carapace/init.nu
       '';
       extraEnv = ''
         sh -c "source /etc/profile"
         sh -c "source ~/.profile"
 
-        use ${nuprompt} panache-git
-        let-env PROMPT_COMMAND = { panache-git }
-        let-env PROMPT_INDICATOR = { "" }
+        # carapace setup
+        mkdir ~/.cache/carapace
+        carapace _carapace nushell | save --force ~/.cache/carapace/init.nu
       '';
     };
   };
