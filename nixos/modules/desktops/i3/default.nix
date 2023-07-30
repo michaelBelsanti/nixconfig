35e8886947bffacb8bfe1b63f1362c20361d4c6e
michaelBelsanti <quasigod-io@protonmail.com>
2023-07-30 02:29:10 -0400

    Migrated picom and betterlockscreen config, updated i3 configs

--- nixos/modules/desktops/i3/default.nix
+++ nixos/modules/desktops/i3/default.nix
@@ -38,49 +38,104 @@
       };
       extraConfig = builtins.readFile ./config;
     };
-    services.dunst = {
-      enable = true;
-      settings = {
-        urgency_low.timeout = 3;
-        urgency_normal.timeout = 8;
-        urgency_critical.timeout = 0;
-        global = {
-          follow = "mouse";
-          width = 350;
-          height = 250;
-          origin = "top-right";
-          offset = "15x40";
-          scale = 0;
-          notification_limit = 0;
-          progress_bar = "true";
-          progress_bar_height = 10;
-          progress_bar_frame_width = 1;
-          progress_bar_min_width = 100;
-          progress_bar_max_width = 150;
-          indicate_hidden = "yes";
-          transparency = 0;
-          separator_height = 2;
-          padding = 10;
-          horizontal_padding = 10;
-          text_icon_padding = 0;
-          frame_width = 3;
-          sort = "yes";
-          font = "Sans 12";
-          line_height = 0;
-          markup = "full";
-          # format = "<b>%a: %s</b>\n%b\n%p"
-          alignment = "left";
-          vertical_alignment = "center";
-          show_age_threshold = 30;
-          browser = "${pkgs.xdg-utils}/bin/xdg-open";
+    services = {
+      betterlockscreen = {
+        enable = true;
+        arguments = ["--blur" "--off 30"];
+      };
+      picom = {
+        enable = true;
+        fade = true;
+        fadeDelta = 2;
+        menuOpacity = 0.8;
+        shadowExclude = [
+          "name = 'Notification'"
+          "class_g = 'Conky'"
+          "class_g ?= 'Notify-osd'"
+          "class_g = 'Cairo-clock'"
+          "class_g = 'slop'"
+          "class_g = 'Polybar'"
+          "_GTK_FRAME_EXTENTS@:c"
+        ];
+        fadeExclude = [
+          "class_g = 'slop'" # maim
+        ];
+        wintypes = {
+          popup_menu = {opacity = 0.8;};
+          dropdown_menu = {opacity = 0.8;};
+        };
+        settings = {
+          backend = "glx";
+          round-borders = 1;
+          corner-radius = 6;
+          rounded-corners-exclude = [
+            "class_g = 'Polybar'"
+            "window_type = 'dock'"
+            "window_type = 'desktop'"
+          ];
+          blur = {
+            method = "dual_kawase";
+            strength = 7;
+            background = true;
+          };
+          blur-background-exclude = [
+            "window_type = 'desktop'"
+            "window_type = 'menu'"
+            "class_g = 'pensela'"
+            "name = 'Discover Text'"
+            "name = 'Discover Voice'"
+            "name = 'peek'"
+            "name = 'Peek'"
+            "class_g = 'firefox' && window_type = 'utility'"
+            "class_g = 'slop'"
+            "_GTK_FRAME_EXTENTS@:c"
+          ];
+        };
+      };
+      dunst = {
+        enable = true;
+        settings = {
+          urgency_low.timeout = 3;
+          urgency_normal.timeout = 8;
+          urgency_critical.timeout = 0;
+          global = {
+            follow = "mouse";
+            width = 350;
+            height = 250;
+            origin = "top-right";
+            offset = "15x40";
+            scale = 0;
+            notification_limit = 0;
+            progress_bar = "true";
+            progress_bar_height = 10;
+            progress_bar_frame_width = 1;
+            progress_bar_min_width = 100;
+            progress_bar_max_width = 150;
+            indicate_hidden = "yes";
+            transparency = 0;
+            separator_height = 2;
+            padding = 10;
+            horizontal_padding = 10;
+            text_icon_padding = 0;
+            frame_width = 3;
+            sort = "yes";
+            font = "Sans 12";
+            line_height = 0;
+            markup = "full";
+            # format = "<b>%a: %s</b>\n%b\n%p"
+            alignment = "left";
+            vertical_alignment = "center";
+            show_age_threshold = 30;
+            browser = "${pkgs.xdg-utils}/bin/xdg-open";
+          };
         };
       };
     };
     xdg.configFile = {
-      picom = {
-        source = ./picom;
-        recursive = true;
-      };
+      # picom = {
+      #   source = ./picom;
+      #   recursive = true;
+      # };
       polybar = {
         source = ./polybar;
         recursive = true;
