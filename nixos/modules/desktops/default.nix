780f696cd2728e84a0711eea0166c6b8acfd976a
michaelBelsanti <quasigod-io@protonmail.com>
2023-08-28 23:51:26 -0400

    Moved polkit from i3 config to service

--- nixos/modules/desktops/default.nix
+++ nixos/modules/desktops/default.nix
@@ -15,14 +15,21 @@
     breeze-icons
     libsForQt5.lightly
     libsForQt5.baloo # dolphin
-
-    (polkit_gnome.overrideAttrs (_oldAttrs: {
-      postFixup = ''
-        mkdir $out/bin
-        ln -s $out/libexec/polkit-gnome-authentication-agent-1 $out/bin/polkit-gnome
-      '';
-    }))
   ];
+
+  systemd.user.services.polkit-gnome-authentication-agent-1 = {
+    description = "polkit-gnome-authentication-agent-1";
+    wantedBy = ["graphical-session.target"];
+    wants = ["graphical-session.target"];
+    after = ["graphical-session.target"];
+    serviceConfig = {
+      Type = "simple";
+      ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+      Restart = "on-failure";
+      RestartSec = 1;
+      TimeoutStopSec = 10;
+    };
+  };
 
   environment.sessionVariables = {
     QT_QPA_PLATFORMTHEME = "qt5ct";
