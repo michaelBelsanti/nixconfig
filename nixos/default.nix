5c2300846a3cad54ed6681b8acbf4bebe45b50e3
michaelBelsanti <quasigod-io@protonmail.com>
2023-09-08 15:50:35 -0400

    Removed unneeded xresources options

--- nixos/default.nix
+++ nixos/default.nix
@@ -46,42 +46,6 @@
       # x11.enable = true;
       gtk.enable = true;
     };
-    # xresources = {
-    #   path = "${config.xdg.configHome}/X11/xresources";
-    #   properties = {
-    #     "*background" = "#1E1D2F";
-    #     "*foreground" = "#D9E0EE";
-    #     "*lightbg" = "#2F2F4C";
-    #     # Grey
-    #     "*color0" = "#6E6C7E";
-    #     "*color8" = "#988BA2";
-    #     # Red
-    #     "*color1" = "#F28FAD";
-    #     "*color9" = "#F28FAD";
-    #     # Green
-    #     "*color2" = "#ABE9B3";
-    #     "*color10" = "#ABE9B3";
-    #     # Yellow
-    #     "*color3" = "#FAE3B0";
-    #     "*color11" = "#FAE3B0";
-    #     # Blue
-    #     "*color4" = "#96CDFB";
-    #     "*color12" = "#96CDFB";
-    #     # Maguve
-    #     "*color5" = "#DDB6F2";
-    #     "*color13" = "#DDB6F2";
-    #     # Pink
-    #     "*color6" = "#F5C2E7";
-    #     "*color14" = "#F5C2E7";
-    #     # Whites
-    #     "*color7" = "#C3BAC6";
-    #     "*color15" = "#D9E0EE";
-    #     # Other (really just for nsxiv)
-    #     "window.background" = "#1E1D2F";
-    #     "window.foreground" = "#DADAE8";
-    #     "bar.font" = "Montserrat 11";
-    #   };
-    # };
     programs.spicetify = {
       enable = true;
       enabledExtensions = with pkgs.spicePkgs.extensions; [fullAppDisplay featureShuffle hidePodcasts];
