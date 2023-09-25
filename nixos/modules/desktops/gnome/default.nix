8fc21b3c666e1922c0574350afecb0b4d49481f1
michaelBelsanti <quasigod-io@protonmail.com>
2023-09-25 19:47:02 -0400

    nixos/gnome: removed old comments

--- nixos/modules/desktops/gnome/default.nix
+++ nixos/modules/desktops/gnome/default.nix
@@ -3,14 +3,12 @@
   user,
   ...
 }: {
-  # Import automatically generated plasma-manager config
-  # Generated using 'rc2nix' or `nix run github:pjones/plasma-manager`
   services.xserver.desktopManager.gnome.enable = true;
   environment.systemPackages = with pkgs; [gnome.gnome-tweaks];
   home-manager.users.${user} = {
     home.packages = with pkgs.gnomeExtensions; [
-      pop-shell
-      space-bar
+      forge
+      # space-bar
     ];
   };
 }
