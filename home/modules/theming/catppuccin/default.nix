97b770a055d7ba77c6a1872c688e038837a43a31
michaelBelsanti <quasigod-io@protonmail.com>
2024-02-13 22:23:39 -0500

    home/catppuccin: removed bat theme

--- home/modules/theming/catppuccin/default.nix
+++ home/modules/theming/catppuccin/default.nix
@@ -212,19 +212,6 @@ in {
             hash = "sha256-frkGtsk/VuS6MYUf7S2hqNHhTaV6S0Mv2UuttCgvimk=";
           }
           + /theme/${flavour}.ron);
-      bat = {
-        config.theme = "Catppuccin";
-        themes = {
-          "Catppuccin" = builtins.readFile (pkgs.fetchFromGitHub
-            {
-              owner = "catppuccin";
-              repo = "bat";
-              rev = "ba4d16880d63e656acced2b7d4e034e4a93f74b1";
-              hash = "sha256-6WVKQErGdaqb++oaXnY3i6/GuH2FhTgK0v4TN4Y0Wbw=";
-            }
-            + "/Catppuccin-${flavour}.tmTheme");
-        };
-      };
     };
   };
 }
