7747a90911526d5651986ff7f6420a60097a71bb
michaelBelsanti <quasigod-io@proton.me>
2024-03-27 14:28:36 -0400

    nixos/catppuccin: removed starship theme
    terminal is already themed, doesnt actually change anything

--- modules/home/theming/catppuccin/default.nix
+++ modules/home/theming/catppuccin/default.nix
@@ -154,23 +154,6 @@ in
       );
       zellij.settings.theme = "catppuccin-${flavour}";
       kitty.theme = "Catppuccin-Macchiato";
-      starship = {
-        settings =
-          {
-            palette = "catppuccin_${flavour}";
-          }
-          // builtins.fromTOML (
-            builtins.readFile (
-              pkgs.fetchFromGitHub {
-                owner = "catppuccin";
-                repo = "starship";
-                rev = "3e3e54410c3189053f4da7a7043261361a1ed1bc";
-                hash = "sha256-soEBVlq3ULeiZFAdQYMRFuswIIhI9bclIU8WXjxd7oY=";
-              }
-              + /palettes/${flavour}.toml
-            )
-          );
-      };
       foot.settings.colors = {
         # Macchiato theme
         foreground = "cad3f5"; # Text
