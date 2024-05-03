c8139f2b4352e41ceaa70734085aa31a37c2518a
michaelBelsanti <quasigod-io@proton.me>
2024-05-03 13:49:43 -0400

    packages: init nexus-mods-app

---
+++ packages/nexus-mods-app/default.nix
@@ -0,0 +1,29 @@
+{
+  appimageTools,
+  fetchurl,
+  lib,
+  # makeWrapper,
+}:
+let
+  pname = "nexus-mods-app";
+  version = "0.4.1";
+
+  src = fetchurl {
+    url = "https://github.com/Nexus-Mods/NexusMods.App/releases/download/v${version}/NexusMods.App.x86_64.AppImage";
+    hash = "sha256-Zvs4EK082s9NE1sj/eonUL6Fr5akUZPeNlvcxCTLQCk=";
+  };
+in
+appimageTools.wrapType2 {
+  inherit pname version src;
+
+  extraPkgs = pkgs: [ pkgs.icu ];
+
+  meta = with lib; {
+    description = "Home of the development of the Nexus Mods App";
+    homepage = "https://github.com/Nexus-Mods/NexusMods.App";
+    changelog = "https://github.com/Nexus-Mods/NexusMods.App/blob/v${version}/CHANGELOG.md";
+    license = licenses.gpl3Only;
+    mainProgram = "nexus-mods";
+    platforms = platforms.all;
+  };
+}
