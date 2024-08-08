6e22f11ec7d620a63e3c666b52c0494b02fa3e6a
michaelBelsanti <quasigod-io@proton.me>
2024-08-08 18:02:14 -0400

    enable theming for quasi@nyx

--- homes/x86_64-linux/quasi@nyx/default.nix
+++ homes/x86_64-linux/quasi@nyx/default.nix
@@ -2,4 +2,8 @@ _: {
   # for sshmux daemon
   apps.wezterm.enable = true;
   programs.starship.settings.hostname.ssh_only = false;
+  theming = {
+    enable = true;
+    theme = "catppuccin";
+  };
 }
