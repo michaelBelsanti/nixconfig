8b575364a8ccb070a2a9461b1fdd7e9b3ba0f93a
michaelBelsanti <quasigod-io@proton.me>
2024-09-30 19:33:38 -0400

    disable git signing

--- homes/x86_64-linux/quasi@hades/default.nix
+++ homes/x86_64-linux/quasi@hades/default.nix
@@ -1,7 +1,7 @@
 _: {
   apps.easyeffects.enable = true;
-  programs.git.signing = {
-    signByDefault = true;
-    key = "CEB0864EF4C8A420";
-  };
+  # programs.git.signing = {
+  #   signByDefault = true;
+  #   key = "CEB0864EF4C8A420";
+  # };
 }
--- homes/x86_64-linux/quasi@zagreus/default.nix
+++ homes/x86_64-linux/quasi@zagreus/default.nix
@@ -1,7 +1,6 @@
 _: {
-  apps.rio.enable = true;
-  programs.git.signing = {
-    signByDefault = true;
-    key = "59B519DCB54BD92E";
-  };
+  # programs.git.signing = {
+  #   signByDefault = false;
+  #   key = "";
+  # };
 }
