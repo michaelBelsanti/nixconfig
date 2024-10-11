048f4094aaa5433b2946cc93bb4b03bc66147d5d
michaelBelsanti <quasigod-io@proton.me>
2024-10-11 18:11:30 -0400

    remove nix-flatpak, add wrapper-manager

--- flake.nix
+++ flake.nix
@@ -142,6 +142,11 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    wrapper-manager = {
+      url = "github:viperML/wrapper-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     lix-module = {
       url = "https://git.lix.systems/lix-project/nixos-module/archive/2.91.0.tar.gz";
       inputs.nixpkgs.follows = "nixpkgs";
