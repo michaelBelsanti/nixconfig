3e4ec23b3fdd67916f6aad09897bd513cb590c21
michaelBelsanti <quasigod-io@proton.me>
2025-01-03 15:15:41 -0500

    update overlay to use infuse, split input inherits into new overlay

---
+++ overlays/inherits/default.nix
@@ -0,0 +1,18 @@
+# Packages to inherit from an input
+{
+  helix,
+  nix-gaming,
+  plasma-manager,
+  nix-alien,
+  hypr-contrib,
+  zen-browser,
+  ...
+}:
+_self: super: {
+  inherit (helix.packages.${super.system}) helix;
+  inherit (plasma-manager.packages.${super.system}) rc2nix;
+  inherit (nix-alien.packages.${super.system}) nix-alien;
+  inherit (hypr-contrib.packages.${super.system}) grimblast;
+  inherit (zen-browser.packages.${super.system}) zen-browser;
+  gaming = nix-gaming.packages.${super.system};
+}
