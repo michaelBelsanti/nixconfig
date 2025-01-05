e26a97eebb68da86cc1c43a1403bc76c99bf8c1e
michaelBelsanti <quasigod-io@proton.me>
2025-01-05 00:02:05 -0500

    remove bottles warning

--- overlays/infuser/default.nix
+++ overlays/infuser/default.nix
@@ -6,15 +6,20 @@ let
  gsr-version = "5.0.0";
 in
 _self: super: lib.custom.infuse super {
-  gpu-screen-recorder.__output.version.__assign = gsr-version;
-  gpu-screen-recorder.__output.src.__assign = super.fetchurl {
-    url = "https://dec05eba.com/snapshot/gpu-screen-recorder.git.${gsr-version}.tar.gz";
-    hash = "sha256-w1dtFLSY71UileoF4b1QLKIHYWPE5c2KmsHyRPtn+sA=";
+  gpu-screen-recorder.__output = {
+    version.__assign = gsr-version;
+    src.__assign = super.fetchurl {
+      url = "https://dec05eba.com/snapshot/gpu-screen-recorder.git.${gsr-version}.tar.gz";
+      hash = "sha256-w1dtFLSY71UileoF4b1QLKIHYWPE5c2KmsHyRPtn+sA=";
+    };
+  };
+  gpu-screen-recorder-gtk.__output = {
+    version.__assign = gsr-version;
+    src.__assign = super.fetchurl {
+      url = "https://dec05eba.com/snapshot/gpu-screen-recorder-gtk.git.${gsr-version}.tar.gz";
+      hash = "sha256-uXbiuA1XPWZVwQGLh47rKzCZSEUEPWqYALqMuCGA7do=";
+    };
   };
 
-  gpu-screen-recorder-gtk.__output.version.__assign = gsr-version;
-  gpu-screen-recorder-gtk.__output.src.__assign = super.fetchurl {
-    url = "https://dec05eba.com/snapshot/gpu-screen-recorder-gtk.git.${gsr-version}.tar.gz";
-    hash = "sha256-uXbiuA1XPWZVwQGLh47rKzCZSEUEPWqYALqMuCGA7do=";
-  };
+  bottles.__input.removeWarningPopup.__assign = true;
 }
