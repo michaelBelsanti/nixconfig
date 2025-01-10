2c9d2b93749085c4b82255c60ec4bce283d5fcd5
michaelBelsanti <quasigod-io@proton.me>
2025-01-09 22:31:47 -0500

    remove gpu-screen-recorder override

--- overlays/infuser/default.nix
+++ overlays/infuser/default.nix
@@ -2,24 +2,7 @@
   lib,
   ...
 }:
-let
- gsr-version = "5.0.0";
-in
-_self: super: lib.custom.infuse super {
-  gpu-screen-recorder.__output = {
-    version.__assign = gsr-version;
-    src.__assign = super.fetchurl {
-      url = "https://dec05eba.com/snapshot/gpu-screen-recorder.git.${gsr-version}.tar.gz";
-      hash = "sha256-w1dtFLSY71UileoF4b1QLKIHYWPE5c2KmsHyRPtn+sA=";
-    };
-  };
-  gpu-screen-recorder-gtk.__output = {
-    version.__assign = gsr-version;
-    src.__assign = super.fetchurl {
-      url = "https://dec05eba.com/snapshot/gpu-screen-recorder-gtk.git.${gsr-version}.tar.gz";
-      hash = "sha256-uXbiuA1XPWZVwQGLh47rKzCZSEUEPWqYALqMuCGA7do=";
-    };
-  };
-
+_self: super:
+lib.custom.infuse super {
   bottles.__input.removeWarningPopup.__assign = true;
 }
