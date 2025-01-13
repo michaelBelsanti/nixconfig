fbd533fe79fdbcb34b13fa3f565b5ebc4b49e504
michaelBelsanti <quasigod-io@proton.me>
2025-01-13 16:04:56 -0500

    init

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  description = "My personal NUR repository";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    snowfall-lib = {
+      url = "github:snowfallorg/lib";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+  outputs = inputs:
+    inputs.snowfall-lib.mkFlake {
+      inherit inputs;
+      src = ./.;
+    };
+}
