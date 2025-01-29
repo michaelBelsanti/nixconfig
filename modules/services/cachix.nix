f67fab3cf9f4d6ef124e4f832523954bb17b3a7f
michaelBelsanti <quasigod-io@proton.me>
2025-01-29 13:10:13 -0500

    add cachix module

---
+++ modules/services/cachix.nix
@@ -0,0 +1,11 @@
+{ delib, host, ... }:
+delib.module {
+  name = "services.cachix";
+  options = delib.singleEnableOption host.isDesktop;
+  nixos.ifEnabled = {
+    services.cachix-watch-store = {
+      enable = true;
+      cacheName = "quasigod";
+    };
+  };
+}
