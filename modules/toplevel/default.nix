b8b5f6eae8de8c0a46007958bc9bdbba9c3fbc5e
michaelBelsanti <quasigod-io@proton.me>
2025-01-30 19:16:07 -0500

    set enableAllTerminfo on servers

--- modules/toplevel/default.nix
+++ modules/toplevel/default.nix
@@ -1,8 +1,9 @@
-{ delib, pkgs, ...}:
+{ delib, pkgs, host, ...}:
 delib.module {
   name = "nixos";
   nixos.always = {
     environment.binsh = "${pkgs.dash}/bin/dash";
+    environment.enableAllTerminfo = host.isServer;
     zramSwap.enable = true;
   };
 }
