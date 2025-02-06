1a608295fc961cb6a95c923bbd842b743b50c421
michaelBelsanti <quasigod-io@proton.me>
2025-02-05 21:10:11 -0500

    removed zed package

--- modules/packages/gui.nix
+++ modules/packages/gui.nix
@@ -44,14 +44,6 @@ delib.module rec {
 
         # Editor
         helix
-        (pkgs.buildFHSEnv {
-          name = "zed";
-          targetPkgs =
-            pkgs: with pkgs; [
-              zed-editor
-            ];
-          runScript = "zed";
-        })
 
         # Containers
         distrobox
