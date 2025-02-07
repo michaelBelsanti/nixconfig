589c2b5aaf0d948fe46896ef338a358b78d3ef00
michaelBelsanti <quasigod-io@proton.me>
2025-02-07 17:01:55 -0500

    update overlay to use infuser

--- modules/overlay.nix
+++ modules/overlay.nix
@@ -1,12 +1,44 @@
 # Packages to inherit from an input
-{ delib, inputs, ... }:
+{
+  delib,
+  inputs,
+  lib,
+  ...
+}:
 let
-  overlay = _self: super: {
+  infuse = (import inputs.infuse.outPath { inherit lib; }).v1.infuse;
+  inherits = _: super: {
     inherit (inputs.nix-alien.packages.${super.system}) nix-alien;
   };
+  overlay =
+    _self: super:
+    infuse super {
+      proton-ge-bin.__output.buildCommand.__assign = ''
+        runHook preBuild
+
+        # Make it impossible to add to an environment. You should use the appropriate NixOS option.
+        # Also leave some breadcrumbs in the file.
+        echo "proton-ge-bin should not be installed into environments. Please use programs.steam.extraCompatPackages instead." > $out
+
+        mkdir $steamcompattool
+        ln -s $src/* $steamcompattool
+        rm $steamcompattool/compatibilitytool.vdf
+        cp $src/compatibilitytool.vdf $steamcompattool
+
+        sed -i -r 's|GE-Proton[0-9]*-[0-9]*|GE-Proton|' $steamcompattool/compatibilitytool.vdf
+
+        runHook postBuild
+      '';
+    };
 in
 delib.module {
   name = "overlay";
-  home.always.nixpkgs.overlays = [ overlay ];
-  nixos.always.nixpkgs.overlays = [ overlay ];
+  home.always.nixpkgs.overlays = [
+    overlay
+    inherits
+  ];
+  nixos.always.nixpkgs.overlays = [
+    overlay
+    inherits
+  ];
 }
