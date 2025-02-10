b96937de7f2f7772d95ded1309326b9ae2608ccb
michaelBelsanti <quasigod-io@proton.me>
2025-02-10 16:49:57 -0500

    enable xdgOpenUsePortal

--- modules/toplevel/xdg.nix
+++ modules/toplevel/xdg.nix
@@ -2,10 +2,13 @@
 delib.module {
   name = "xdg";
   options = delib.singleEnableOption host.isWorkstation;
-  nixos.ifEnabled.xdg.terminal-exec.enable = true;
-  home.ifEnabled = {
-    xdg.enable = true;
-    xdg.userDirs = {
+  nixos.ifEnabled.xdg = {
+    terminal-exec.enable = true;
+    portal.xdgOpenUsePortal = true;
+  };
+  home.ifEnabled.xdg = {
+    enable = true;
+    userDirs = {
       enable = true;
       createDirectories = true;
       desktop = null;
