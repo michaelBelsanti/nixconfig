96ec44853da6720007f03a3c9aff1590ecf77cd4
michaelBelsanti <quasigod-io@proton.me>
2025-02-16 17:24:46 -0500

    added eternal-terminal option to sshd module

--- modules/services/sshd.nix
+++ modules/services/sshd.nix
@@ -1,25 +1,31 @@
 { delib, host, ... }:
 delib.module {
   name = "services.sshd";
-  options = delib.singleEnableOption host.isServer;
-  nixos.ifEnabled = {
-    services.openssh = {
-      enable = true;
-      settings = {
-        # following ssh-audit: nixos default minus 2048 bit modules
-        KexAlgorithms = [
-          "sntrup761x25519-sha512@openssh.com"
-          "curve25519-sha256"
-          "curve25519-sha256@libssh.org"
-        ];
-        # following ssh-audit: nixos defaults minus encrypt-and-MAC
-        Macs = [
-          "hmac-sha2-512-etm@openssh.com"
-          "hmac-sha2-256-etm@openssh.com"
-          "umac-128-etm@openssh.com"
-        ];
-        RequiredRSASize = 4095;
+  options.services.sshd = {
+    enable = delib.boolOption host.isServer;
+    eternal-terminal.enable = delib.boolOption host.isServer;
+  };
+  nixos.ifEnabled =
+    { cfg, ... }:
+    {
+      services.eternal-terminal.enable = cfg.eternal-terminal.enable;
+      services.openssh = {
+        enable = true;
+        settings = {
+          # following ssh-audit: nixos default minus 2048 bit modules
+          KexAlgorithms = [
+            "sntrup761x25519-sha512@openssh.com"
+            "curve25519-sha256"
+            "curve25519-sha256@libssh.org"
+          ];
+          # following ssh-audit: nixos defaults minus encrypt-and-MAC
+          Macs = [
+            "hmac-sha2-512-etm@openssh.com"
+            "hmac-sha2-256-etm@openssh.com"
+            "umac-128-etm@openssh.com"
+          ];
+          RequiredRSASize = 4095;
+        };
       };
     };
-  };
 }
