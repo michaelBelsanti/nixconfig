07dfd56def93524751544c639e3193dbba20fdbf
quasigod <quasigod-io@proton.me>
2025-02-22 17:55:54 -0500

    add aliases

--- modules/programs/shells/default.nix
+++ modules/programs/shells/default.nix
@@ -6,21 +6,21 @@
 }:
 delib.module {
   name = "programs.shells";
-  nixos.always.environment.shells = [ pkgs.bash ];
-  home.always =
-
-    {
-      home.shellAliases = {
-        cd = "z";
-        lg = "lazygit";
-        open = "xdg-open";
-        mkdir = "mkdir -p";
-        tree = "eza -T";
-      };
-      programs.bash = {
-        enable = true;
-        enableVteIntegration = true;
-        historyFile = "${constants.dataHome}/bash/history";
-      };
+  nixos.always.environment.shells = [ pkgs.bashInteractive ];
+  home.always = {
+    home.shellAliases = {
+      cd = "z";
+      lj = "lazyjj";
+      lg = "lazygit";
+      open = "xdg-open";
+      mkdir = "mkdir -p";
+      tree = "eza -T";
+      get = "aria2c";
     };
+    programs.bash = {
+      enable = true;
+      enableVteIntegration = true;
+      historyFile = "${constants.dataHome}/bash/history";
+    };
+  };
 }
