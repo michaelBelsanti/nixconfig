fc22e4f1466b6fe9eb0472bc389a87e25644cfaa
quasigod <quasigod-io@proton.me>
2025-02-27 17:53:41 -0500

    add sops home-manager config

--- modules/secrets/default.nix
+++ modules/secrets/default.nix
@@ -5,19 +5,24 @@
   pkgs,
   ...
 }:
+let
+  sops_config = {
+    defaultSopsFile = ./secrets.yaml;
+    defaultSopsFormat = "yaml";
+    age.keyFile = "${constants.configHome}/sops/age/keys.txt";
+  };
+in
 delib.module {
   name = "secrets";
   options = delib.singleEnableOption true;
-  home.always.home = {
-    sessionVariables.SOPS_AGE_KEY_FILE = "${constants.configHome}/sops/age/keys.txt";
-    packages = [ pkgs.sops ];
+  home.always = {
+    imports = [ inputs.sops-nix.homeManagerModules.sops ];
+    sops = sops_config;
+    home.sessionVariables.SOPS_AGE_KEY_FILE = "${constants.configHome}/sops/age/keys.txt";
+    home.packages = [ pkgs.sops ];
   };
   nixos.always = {
     imports = [ inputs.sops-nix.nixosModules.sops ];
-    sops = {
-      defaultSopsFile = ./secrets.yaml;
-      defaultSopsFormat = "yaml";
-      age.keyFile = "${constants.configHome}/sops/age/keys.txt";
-    };
+    sops = sops_config;
   };
 }
