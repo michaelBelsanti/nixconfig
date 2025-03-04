de5043218fd859408441fb0c0161aebb82422877
quasigod <quasigod-io@proton.me>
2025-03-03 22:05:23 -0500

    add jji alias

--- modules/programs/jujutsu.nix
+++ modules/programs/jujutsu.nix
@@ -2,16 +2,19 @@
 delib.module {
   name = "programs.jujutsu";
   options = delib.singleEnableOption true;
-  home.ifEnabled.programs.jujutsu = {
-    enable = true;
-    settings = {
-      user = {
-        name = "quasigod";
-        email = "quasigod-io@proton.me";
-      };
-      ui = {
-        paginate = "never";
-        default-command = "log";
+  home.ifEnabled = {
+    home.shellAliases.jji = "jj --ignore-immutable";
+    programs.jujutsu = {
+      enable = true;
+      settings = {
+        user = {
+          name = "quasigod";
+          email = "quasigod-io@proton.me";
+        };
+        ui = {
+          paginate = "never";
+          default-command = "log";
+        };
       };
     };
   };
