e93ed94ff8687f69fd1a2858f5f3d78cb1a335b4
quasigod <quasigod-io@proton.me>
2025-03-13 09:48:57 -0400

    output denix modules

--- modules/services/flatpak.nix
+++ modules/services/flatpak.nix
@@ -1,13 +1,11 @@
 {
   delib,
   host,
-  inputs,
   ...
 }:
 delib.module {
   name = "services.flatpak";
   options = delib.singleEnableOption host.isWorkstation;
-  home.ifEnabled.imports = [ inputs.nix-flatpak.homeManagerModules.nix-flatpak ];
   nixos.ifEnabled = {
     appstream.enable = true;
     services.flatpak.enable = true;
