06ed46f6694c0cb69def60d2315e060122664218
quasigod <quasigod-io@proton.me>
2025-03-19 11:34:54 -0400

    move more server config out of this repo
    moved to a private repo to simplify my workflow

--- hosts/nyx/default.nix
+++ hosts/nyx/default.nix
@@ -1,9 +1,4 @@
-{
-  delib,
-  inputs,
-  pkgs,
-  ...
-}:
+{ delib, ... }:
 delib.host {
   name = "nyx";
   type = "server";
@@ -12,49 +7,4 @@ delib.host {
 
   shared.myconfig.services.syncthing.devices.nyx.id =
     "T7ES5DM-TIODWXE-T2LME4T-3RKTD6S-WFVTSVS-QHU5WEW-Q6GDROS-46H2AQZ";
-
-  myconfig = {
-    virtualisation.enable = false;
-    packages.hacking.enable = false;
-    zfs.pools = [ "zfsa" ];
-  };
-
-  nixos = {
-    nixpkgs.hostPlatform = "x86_64-linux";
-    imports = with inputs; [
-      nixos-hardware.nixosModules.common-cpu-amd
-      nixos-hardware.nixosModules.common-gpu-intel
-      nixos-hardware.nixosModules.common-pc-ssd
-    ];
-    facter.reportPath = ./facter.json;
-    hardware = {
-      graphics.enable = true;
-      intelgpu = {
-        vaapiDriver = null;
-        enableHybridCodec = true;
-      };
-    };
-    boot.kernelPackages = pkgs.linuxPackages_6_11;
-    boot.loader.efi.efiSysMountPoint = "/boot/efi";
-    networking = {
-      hostName = "nyx";
-      hostId = "18190aed";
-      defaultGateway = {
-        address = "192.168.1.1";
-        interface = "enp42s0";
-      };
-      nameservers = [ "1.1.1.1" ];
-      interfaces.enp42s0.ipv4.addresses = [
-        {
-          address = "192.168.1.192";
-          prefixLength = 24;
-        }
-      ];
-      nat = {
-        enable = true;
-        internalInterfaces = [ "ve-+" ];
-        externalInterface = "enp42s0";
-      };
-    };
-  };
 }
--- modules/toplevel/virtualisation.nix
+++ modules/toplevel/virtualisation.nix
@@ -3,34 +3,40 @@
   lib,
   pkgs,
   constants,
+  host,
   ...
 }:
 delib.module {
   name = "virtualisation";
-  options = delib.singleEnableOption true;
-  nixos.ifEnabled = {
-    boot.kernelParams = [ "amd_iommu=on" ];
-    users.users.${constants.username}.extraGroups = [ "kvm" ];
-    programs.virt-manager.enable = true;
-    environment.systemPackages = with pkgs; [
-      gnome-boxes
-      virglrenderer
-    ];
-    services.qemuGuest.enable = true;
-    virtualisation = {
-      waydroid.enable = true;
-      libvirtd.enable = true;
-      spiceUSBRedirection.enable = true;
-      podman.enable = true;
-      vmVariant = {
-        services.btrfs.autoScrub.enable = lib.mkForce false;
-        virtualisation = {
-          qemu.guestAgent.enable = true;
-          memorySize = 6144;
-          diskSize = 10240;
-          cores = 4;
-        };
+  options.virtualisation = {
+    enable = delib.boolOption host.isWorkstation;
+    waydroid.enable = delib.boolOption false;
+  };
+  nixos.ifEnabled =
+    { cfg, ... }:
+    {
+      boot.kernelParams = [ "amd_iommu=on" ];
+      users.users.${constants.username}.extraGroups = [ "kvm" ];
+      programs.virt-manager.enable = true;
+      environment.systemPackages = with pkgs; [
+        gnome-boxes
+        virglrenderer
+      ];
+      services.qemuGuest.enable = true;
+      virtualisation = {
+        waydroid.enable = cfg.waydroid.enable;
+        libvirtd.enable = true;
+        spiceUSBRedirection.enable = true;
+        podman.enable = true;
       };
+    };
+  nixos.always.virtualisation.vmVariant = {
+    services.btrfs.autoScrub.enable = lib.mkForce false;
+    virtualisation = {
+      qemu.guestAgent.enable = true;
+      memorySize = 6144;
+      diskSize = 10240;
+      cores = 4;
     };
   };
 }
