bd85f9fb294c6b3bdf83570edd7bed58287c3c42
quasigod <quasigod-io@proton.me>
2025-03-26 13:18:58 -0400

    use most recent mpris-scrobbler

--- modules/programs/scrobbler.nix
+++ modules/programs/scrobbler.nix
@@ -1,23 +1,35 @@
 {
   delib,
-  lib,
   pkgs,
   host,
+  lib,
   ...
 }:
 delib.module {
   name = "programs.scrobbler";
   options = delib.singleEnableOption host.isWorkstation;
-  home.ifEnabled = {
-    systemd.user.services.scrobbler = {
-      Unit.Description = "scrobbler background service";
-      Install.WantedBy = [ "graphical-session.target" ];
-      Service = {
-        ExecStart = "${lib.getExe pkgs.mpris-scrobbler} -v";
-        Restart = "on-failure";
-        Environment = "XDG_DATA_HOME=%h/.local/share";
-        PassEnvironment = "PROXY";
+  home.ifEnabled =
+    let
+      mpris-scrobbler = pkgs.mpris-scrobbler.overrideAttrs {
+        src = pkgs.fetchFromGitHub {
+          owner = "mariusor";
+          repo = "mpris-scrobbler";
+          rev = "da3442b7815264b1845b83b05c56bf1d3af5c324";
+          hash = "sha256-ZIRkcIKQVOYkj4Bus30E1jdLZc5kTanS6U+UZYAJXag=";
+        };
+      };
+    in
+    {
+      home.packages = [ mpris-scrobbler ];
+      systemd.user.services.scrobbler = {
+        Unit.Description = "scrobbler background service";
+        Install.WantedBy = [ "graphical-session.target" ];
+        Service = {
+          ExecStart = "${lib.getExe mpris-scrobbler} -v";
+          Restart = "on-failure";
+          Environment = "XDG_DATA_HOME=%h/.local/share";
+          PassEnvironment = "PROXY";
+        };
       };
     };
-  };
 }
