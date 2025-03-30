00305e71e8f1ddb101350832ec467b3f8dd960e6
quasigod <quasigod-io@proton.me>
2025-03-30 12:10:47 -0400

    update easyeffects module to use extraPresets

--- modules/programs/easyeffects/default.nix
+++ modules/programs/easyeffects/default.nix
@@ -2,11 +2,28 @@
 delib.module {
   name = "programs.easyeffects";
   options = delib.singleEnableOption host.isWorkstation;
-  home.ifEnabled = {
-    services.easyeffects.enable = true;
-    xdg.configFile."easyeffects" = {
-      source = ./config;
-      recursive = true;
-    };
+  home.ifEnabled.services.easyeffects = {
+    enable = true;
+    extraPresets =
+      let
+        presets = [
+          "AKG K7XX"
+          "Hifiman Edition XS"
+          "Moondrop Starfield"
+          "Nothing Ear (a)"
+          "7RYMS"
+          "Yeti"
+        ];
+
+        mkPresets =
+          names:
+          builtins.listToAttrs (
+            map (name: {
+              name = name;
+              value = builtins.fromJSON (builtins.readFile (./presets + "/${name}.json"));
+            }) names
+          );
+      in
+      mkPresets presets;
   };
 }
