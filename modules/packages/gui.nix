f5fe59882edd68b37fe5a500a8a466462826179c
quasigod <quasigod-io@proton.me>
2025-04-11 16:05:35 -0400

    add warpd package

--- modules/packages/gui.nix
+++ modules/packages/gui.nix
@@ -3,31 +3,34 @@
   pkgs,
   inputs,
   host,
+  config,
   ...
 }:
 delib.module {
   name = "packages.gui";
   options = delib.singleEnableOption host.isWorkstation;
   home.ifEnabled = {
-    home.packages =
-      with pkgs;
-      [
-        mullvad
-        libreoffice
-        hunspell
-        hunspellDicts.en_US-large
-        remmina
-        (bottles.override { removeWarningPopup = true; })
-        varia
-        proton-pass
-        jellyfin-media-player
-        element-desktop
-        gnome-frog
-        legcord
-        obsidian
-      ];
-      # ++ (with inputs; [
-      #   mypkgs.packages.${system}.grayjay
-      # ]);
+    home.packages = with pkgs; [
+      mullvad
+      libreoffice
+      hunspell
+      hunspellDicts.en_US-large
+      remmina
+      (bottles.override { removeWarningPopup = true; })
+      varia
+      proton-pass
+      jellyfin-media-player
+      element-desktop
+      gnome-frog
+      legcord
+      obsidian
+      (warpd.override {
+        withWayland = config.myconfig.desktops.wayland;
+        withX = !config.myconfig.desktops.wayland;
+      })
+    ];
+    # ++ (with inputs; [
+    #   mypkgs.packages.${system}.grayjay
+    # ]);
   };
 }
