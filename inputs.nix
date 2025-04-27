b9a4ff940dfb0b5c2d6eb2be07d43ca2ff4be36a
quasigod <quasigod-io@proton.me>
2025-04-27 11:29:35 -0400

    move inputs to separate file, add missing pins

---
+++ inputs.nix
@@ -0,0 +1,27 @@
+{ pins }:
+{
+  config.inputs = {
+    nixpkgs.src = pins.nixpkgs;
+    mypkgs.src = pins.mypkgs;
+    unify.src = pins.unify;
+    home-manager.src = pins.home-manager;
+    nixos-hardware.src = pins.nixos-hardware;
+    nixos-facter-modules.src = pins.nixos-facter-modules;
+    chaotic.src = pins.chaotic;
+    wrapper-manager.src = pins.wrapper-manager;
+    nixpak.src = pins.nixpak;
+    nix-colors.src = pins.nix-colors;
+    catppuccin.src = pins.catppuccin;
+    nix-gaming.src = pins.nix-gaming;
+    nixos-cosmic.src = pins.nixos-cosmic;
+    zen-browser.src = pins.zen-browser;
+    nix-alien.src = pins.nix-alien;
+    nix-index-database.src = pins.nix-index-database;
+    lix-module.src = pins.lix-module;
+    lanzaboote.src = pins.lanzaboote;
+    infuse.src = pins.infuse;
+    sops-nix.src = pins.sops-nix;
+    nilla-cli.src = pins.nilla-cli;
+    nilla-utils.src = pins.nilla-utils;
+  };
+}
--- nilla.nix
+++ nilla.nix
@@ -8,13 +8,23 @@ nilla.create (
     includes = [
       "${pins.nilla-utils}/modules/nixos.nix"
       "${pins.unify}/unify.nix"
+      ./inputs.nix
     ];
     config =
       let
         inputs = builtins.mapAttrs (name: value: value.result) config.inputs;
       in
       {
+        __module__.args.dynamic.pins = pins;
         lib = inputs.unify.lib;
+        shells.default = {
+          systems = [ "x86_64-linux" ];
+          shell =
+            { mkShell, system }:
+            mkShell {
+              packages = [ inputs.nilla-utils.packages.default.result.${system} ];
+            };
+        };
         unify.hosts =
           let
             hostDefaults = {
@@ -36,32 +46,6 @@ nilla.create (
             hades = hostDefaults;
             zagreus = hostDefaults;
           };
-        inputs = {
-          nixpkgs.src = pins.nixpkgs;
-          # nixpkgs-master.src = pins.nixpkgs-master;
-          mypkgs.src = pins.mypkgs;
-          unify.src = pins.unify;
-          denix.src = pins.denix;
-          home-manager.src = pins.home-manager;
-          nixos-hardware.src = pins.nixos-hardware;
-          nixos-facter-modules.src = pins.nixos-facter-modules;
-          chaotic.src = pins.chaotic;
-          wrapper-manager.src = pins.wrapper-manager;
-          nixpak.src = pins.nixpak;
-          nix-colors.src = pins.nix-colors;
-          catppuccin.src = pins.catppuccin;
-          nix-gaming.src = pins.nix-gaming;
-          nixos-cosmic.src = pins.nixos-cosmic;
-          zen-browser.src = pins.zen-browser;
-          nix-alien.src = pins.nix-alien;
-          nix-index-database.src = pins.nix-index-database;
-          lix-module.src = pins.lix-module;
-          lanzaboote.src = pins.lanzaboote;
-          infuse.src = pins.infuse;
-          sops-nix.src = pins.sops-nix;
-          nilla-cli.src = pins.nilla-cli;
-          nilla-utils.src = pins.nilla-utils;
-        };
       };
   }
 )
