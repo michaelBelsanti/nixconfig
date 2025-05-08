00f5549cadfe5844af1fe2569d5003030eea4ed3
quasigod <quasigod-io@proton.me>
2025-05-08 19:44:10 -0400

    wrap nushell.plugins with mkCompat

--- modules/programs/nushell.nix
+++ modules/programs/nushell.nix
@@ -8,39 +8,45 @@
         users.users.${constants.user}.shell = pkgs.nushell;
       };
     home =
-      { pkgs, ... }:
+      { pkgs, mkCompat, ... }:
       {
         programs = {
-          nushell = {
-            enable = true;
-            shellAliases = {
-              l = "ls";
-              la = "ls -a";
-              ll = "ls -al";
-              lg = "lazygit";
-              o = "xdg-open";
-              cd = "z";
-              # overrides for global aliases
-              mkdir = lib.mkForce "mkdir";
-              open = lib.mkForce "open";
-            };
-            plugins = with pkgs.nushellPlugins; [
-              # highlight
-              # net
-              # query
-              # skim
-              # units
-            ];
-            extraConfig = ''
-              $env.config = {
-                show_banner: false
-                display_errors: {
-                    exit_code: false
-                    termination_signal: true
-                }
+          nushell =
+            mkCompat
+              {
+                # TODO reenable when fixed
+                plugins = with pkgs.nushellPlugins; [
+                  # highlight
+                  # net
+                  # query
+                  # skim
+                  # units
+                ];
+
               }
-            '';
-          };
+              {
+                enable = true;
+                shellAliases = {
+                  l = "ls";
+                  la = "ls -a";
+                  ll = "ls -al";
+                  lg = "lazygit";
+                  o = "xdg-open";
+                  cd = "z";
+                  # overrides for global aliases
+                  mkdir = lib.mkForce "mkdir";
+                  open = lib.mkForce "open";
+                };
+                extraConfig = ''
+                  $env.config = {
+                    show_banner: false
+                    display_errors: {
+                        exit_code: false
+                        termination_signal: true
+                    }
+                  }
+                '';
+              };
           carapace = {
             enable = true;
             enableNushellIntegration = true;
