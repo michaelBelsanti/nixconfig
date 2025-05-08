bc4a9de7987be500eacf07a424dc9ae59e7a424d
quasigod <quasigod-io@proton.me>
2025-05-08 19:34:04 -0400

    only enable zen on workstations

--- modules/programs/zen.nix
+++ modules/programs/zen.nix
@@ -1,6 +1,6 @@
 { inputs, ... }:
 {
-  unify.home = {
+  unify.modules.workstation.home = {
     imports = [ inputs.zen-browser.homeModules.default ];
     programs.zen-browser.enable = true;
     home.sessionVariables.BROWSER = "zen";
