8c165004c103e12bdd8b915346467d51be138cdf
quasigod <quasigod-io@proton.me>
2025-05-13 18:11:54 -0400

    fix nushell mkCompat usage, reenable fixed plugin

--- modules/programs/nushell.nix
+++ modules/programs/nushell.nix
@@ -11,42 +11,40 @@
       { pkgs, mkCompat, ... }:
       {
         programs = {
+          # TODO reenable when fixed
           nushell =
-            mkCompat
-              {
-                # TODO reenable when fixed
-                plugins = with pkgs.nushellPlugins; [
-                  # highlight
-                  # net
-                  # query
-                  # skim
-                  # units
-                ];
-
-              }
-              {
-                enable = true;
-                shellAliases = {
-                  l = "ls";
-                  la = "ls -a";
-                  ll = "ls -al";
-                  lg = "lazygit";
-                  o = "xdg-open";
-                  cd = "z";
-                  # overrides for global aliases
-                  mkdir = lib.mkForce "mkdir";
-                  open = lib.mkForce "open";
-                };
-                extraConfig = ''
-                  $env.config = {
-                    show_banner: false
-                    display_errors: {
-                        exit_code: false
-                        termination_signal: true
-                    }
-                  }
-                '';
+            mkCompat {
+              plugins = with pkgs.nushellPlugins; [
+                highlight
+                net
+                # query
+                # skim
+                # units
+              ];
+            } { }
+            // {
+              enable = true;
+              shellAliases = {
+                l = "ls";
+                la = "ls -a";
+                ll = "ls -al";
+                lg = "lazygit";
+                o = "xdg-open";
+                cd = "z";
+                # overrides for global aliases
+                mkdir = lib.mkForce "mkdir";
+                open = lib.mkForce "open";
               };
+              extraConfig = ''
+                $env.config = {
+                  show_banner: false
+                  display_errors: {
+                      exit_code: false
+                      termination_signal: true
+                  }
+                }
+              '';
+            };
           carapace = {
             enable = true;
             enableNushellIntegration = true;
