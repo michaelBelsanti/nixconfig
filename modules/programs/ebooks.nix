b0bb6552d842c0bfc1fc060aa8400cbb96d6fe6c
quasigod <quasigod-io@proton.me>
2025-06-14 13:12:14 -0400

    add kcc to ebooks module

--- modules/programs/ebooks.nix
+++ modules/programs/ebooks.nix
@@ -5,12 +5,14 @@
     {
       home.packages = with pkgs; [
         calibre
+        kcc
+        p7zip # for kcc
         (inputs.wrapper-manager.lib.build {
           inherit pkgs;
           modules = lib.singleton {
             wrappers.hakuneko = {
               basePackage = pkgs.hakuneko;
-              flags = [ "--no-sandbox" ];
+              flags = [ "--no-sandbox" ]; # gpu errors without
             };
           };
         })
