91f569c616516877f1d8dd3d59d7e7e52c175935
quasigod <quasigod-io@proton.me>
2025-06-16 14:34:13 -0400

    add gnupg to jujutsu module

--- modules/programs/jujutsu.nix
+++ modules/programs/jujutsu.nix
@@ -1,27 +1,30 @@
 {
-  unify.home = {
-    home.shellAliases.jji = "jj --ignore-immutable";
-    programs.jujutsu = {
-      enable = true;
-      settings = {
-        user = {
-          name = "quasigod";
-          email = "quasigod-io@proton.me";
-        };
-        ui = {
-          default-command = [
-            "log"
-            "--no-pager"
-          ];
-          show-cryptographic-signatures = true;
-        };
-        git.private-commits = "description(glob:'private:*')";
-        signing = {
-          behavior = "own";
-          backend = "ssh";
-          key = "~/.ssh/id_ed25519.pub";
+  unify.home =
+    { pkgs, ... }:
+    {
+      home.shellAliases.jji = "jj --ignore-immutable";
+      home.packages = [ pkgs.gnupg ];
+      programs.jujutsu = {
+        enable = true;
+        settings = {
+          user = {
+            name = "quasigod";
+            email = "quasigod-io@proton.me";
+          };
+          ui = {
+            default-command = [
+              "log"
+              "--no-pager"
+            ];
+            show-cryptographic-signatures = true;
+          };
+          git.private-commits = "description(glob:'private:*')";
+          signing = {
+            behavior = "own";
+            backend = "ssh";
+            key = "~/.ssh/id_ed25519.pub";
+          };
         };
       };
     };
-  };
 }
