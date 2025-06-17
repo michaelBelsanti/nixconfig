dd703763439878e065b335ececffcdcd5e9dae2d
quasigod <quasigod-io@proton.me>
2025-06-17 13:06:24 -0400

    install wezterm on servers

--- modules/programs/wezterm/default.nix
+++ modules/programs/wezterm/default.nix
@@ -1,9 +1,16 @@
 {
-  unify.modules.workstation = {
-    nixos.xdg.terminal-exec.settings.default = [ "org.wezfurlong.wezterm.desktop" ];
-    home.programs.wezterm = {
-      enable = true;
-      extraConfig = builtins.readFile ./config.lua;
+  unify.modules = {
+    workstation = {
+      nixos.xdg.terminal-exec.settings.default = [ "org.wezfurlong.wezterm.desktop" ];
+      home.programs.wezterm = {
+        enable = true;
+        extraConfig = builtins.readFile ./config.lua;
+      };
     };
+    server.nixos =
+      { pkgs, ... }:
+      {
+        environment.systemPackages = [ pkgs.wezterm ];
+      };
   };
 }
