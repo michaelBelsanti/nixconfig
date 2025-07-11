f8aa82541593c7e9ee5d18778296b50a4dca355e
quasigod <quasigod-io@proton.me>
2025-07-11 13:18:00 -0400

    tailscaled hardening

--- modules/toplevel/networking.nix
+++ modules/toplevel/networking.nix
@@ -21,6 +21,33 @@
         # nixpkgs #180175
         systemd.services.NetworkManager-wait-online.enable = false;
         systemd.network.wait-online.enable = false;
+        systemd.services.tailscaled.serviceConfig = {
+          DeviceAllow = [
+            "/dev/tun"
+            "/dev/net/tun"
+          ];
+          AmbientCapabilities = "CAP_NET_RAW CAP_NET_ADMIN CAP_SYS_MODULE";
+          ProtectKernelModules = "no";
+          RestrictAddressFamilies = "AF_UNIX AF_INET AF_INET6 AF_NETLINK";
+          NoNewPrivileges = "yes";
+          PrivateTmp = "yes";
+          PrivateMounts = "yes";
+          RestrictNamespaces = "yes";
+          RestrictRealtime = "yes";
+          RestrictSUIDSGID = "yes";
+          MemoryDenyWriteExecute = "yes";
+          LockPersonality = "yes";
+          ProtectHome = "yes";
+          ProtectControlGroups = "yes";
+          ProtectKernelLogs = "yes";
+          ProtectSystem = "full";
+          ProtectProc = "noaccess";
+          SystemCallArchitectures = "native";
+          SystemCallFilter = [
+            "@known"
+            "~@clock @cpu-emulation @raw-io @reboot @mount @obsolete @swap @debug @keyring @mount @pkey"
+          ];
+        };
       };
   };
 }
