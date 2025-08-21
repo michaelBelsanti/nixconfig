2c61b7f6952e84c90fad9cbbd22e06b65eaec1df
quasigod <quasigod-io@proton.me>
2025-08-21 19:27:31 -0400

    dont use temp ipv6 address on servers

--- modules/toplevel/networking.nix
+++ modules/toplevel/networking.nix
@@ -10,5 +10,6 @@
       systemd.services.NetworkManager-wait-online.enable = false;
       systemd.network.wait-online.enable = false;
     };
+    modules.server.nixos.networking.tempAddresses = "disabled";
   };
 }
