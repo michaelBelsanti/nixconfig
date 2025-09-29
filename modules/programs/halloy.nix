fe4eee2181105b517636d653f8d1209cfeee7ae2
quasigod <quasigod-io@proton.me>
2025-09-29 02:14:06 -0400

    add halloy module

---
+++ modules/programs/halloy.nix
@@ -0,0 +1,22 @@
+{
+  unify.modules.workstation.home =
+    { config, ... }:
+    {
+      sops.secrets.libera_pass = { };
+      programs.halloy = {
+        enable = true;
+        settings = {
+          font.family = "monospace";
+          servers.liberachat = {
+            nickname = "quasigod";
+            server = "irc.libera.chat";
+            nick_password_file = config.sops.secrets.libera_pass.path;
+            channels = [
+              "#halloy"
+              "#tangled"
+            ];
+          };
+        };
+      };
+    };
+}
