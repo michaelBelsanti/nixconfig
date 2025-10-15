dd21ba2074ad369d0db770312050289ced997bc4
quasigod <quasigod-io@proton.me>
2025-10-14 21:32:18 -0400

    disable some uneeded documentation.* options

--- modules/toplevel/default.nix
+++ modules/toplevel/default.nix
@@ -1,14 +1,21 @@
 {
   unify.nixos =
-    { pkgs, lib, homeConfig, ... }:
+    {
+      pkgs,
+      lib,
+      homeConfig,
+      ...
+    }:
     {
       environment = {
         binsh = "${pkgs.dash}/bin/dash";
         # fixes some issues, mainly root $PATH
         systemPackages = homeConfig.home.packages;
-        defaultPackages = lib.mkForce [];
+        defaultPackages = lib.mkForce [ ];
       };
       zramSwap.enable = true;
+      documentation.doc.enable = false;
+      documentation.info.enable = false;
       time.timeZone = "America/New_York";
       i18n.defaultLocale = "en_US.UTF-8";
       i18n.supportedLocales = [ "all" ];
