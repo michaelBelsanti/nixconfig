7cda241f34f57274e0934c3e56f7465e35c9cb07
quasigod <quasigod-io@proton.me>
2025-10-22 16:30:04 -0400

    update git config to new options

--- modules/programs/git.nix
+++ modules/programs/git.nix
@@ -1,28 +1,23 @@
 {
   unify.home = {
+    programs.difftastic.enable = true;
     programs.git = {
       enable = true;
       lfs.enable = true;
-      userName = "quasigod";
-      userEmail = "quasigod-io@proton.me";
-      difftastic.enable = true;
-      aliases = {
-        ci = "commit";
-        co = "checkout";
-        st = "status";
-      };
-      signing = {
-        format = "ssh";
-        key = "~/.ssh/id_ed25519.pub";
-        signByDefault = true;
-      };
-      extraConfig = {
+      settings = {
+        user.name = "quasigod";
+        user.email = "quasigod-io@proton.me";
         init.defaultBranch = "main";
         pull.rebase = true;
         rerere.enabled = true;
         column.ui = "auto";
         fetch.prune = true;
         interactive.singlekey = true;
+      };
+      signing = {
+        format = "ssh";
+        key = "~/.ssh/id_ed25519.pub";
+        signByDefault = true;
       };
     };
   };
