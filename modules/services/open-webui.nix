4e7d05315e634e32d3c822be17a212bff0879b68
quasigod <quasigod-io@proton.me>
2025-10-22 14:48:25 -0400

    remove open-webui

--- modules/services/open-webui.nix
+++
@@ -1,9 +0,0 @@
-{
-  unify.modules.localai.nixos = {
-    services.open-webui = {
-      enable = true;
-      port = 8008;
-      environment.WEBUI_AUTH = "False"; # only used locally
-    };
-  };
-}
