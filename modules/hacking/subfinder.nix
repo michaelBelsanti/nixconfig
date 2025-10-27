1feaad3cae3ae3c7ffea0705c77897f80897e0c2
quasigod <quasigod-io@proton.me>
2025-10-27 17:39:06 -0400

    add more subfinder keys, move packages to hacking/

---
+++ modules/hacking/subfinder.nix
@@ -0,0 +1,20 @@
+{
+  unify.modules.hacking.home =
+    { lib, config, ... }:
+    {
+      sops.secrets = {
+        shodan_api_key = { };
+        certspotter_api_key = { };
+        chaos_api_key = { };
+        fofa_api_key = { };
+        virustotal_api_key = { };
+      };
+      sops.templates."subfinder-providers.yaml".content = lib.generators.toYAML { } {
+        shodan = [ config.sops.placeholder.shodan_api_key ];
+        certspotter = [ config.sops.placeholder.certspotter_api_key ];
+        chaos = [ config.sops.placeholder.chaos_api_key ];
+        fofa = [ config.sops.placeholder.fofa_api_key ];
+        virustotal = [ config.sops.placeholder.virustotal_api_key ];
+      };
+    };
+}
