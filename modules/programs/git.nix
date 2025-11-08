23ec9c080e2421efccb8f62c8d680ddd6c6714bd
quasigod <quasigod-io@proton.me>
2025-11-08 17:22:21 -0500

    add mkCompat to git module

--- modules/programs/git.nix
+++ modules/programs/git.nix
@@ -1,12 +1,8 @@
 {
-  unify.home = {
-    programs.difftastic.enable = true;
-    programs.git = {
-      enable = true;
-      lfs.enable = true;
-      settings = {
-        user.name = "quasigod";
-        user.email = "quasigod-io@proton.me";
+  unify.home =
+    { lib, mkCompat, ... }:
+    let
+      git_settings = {
         init.defaultBranch = "main";
         pull.rebase = true;
         rerere.enabled = true;
@@ -14,11 +10,40 @@
         fetch.prune = true;
         interactive.singlekey = true;
       };
-      signing = {
-        format = "ssh";
-        key = "~/.ssh/id_ed25519.pub";
-        signByDefault = true;
-      };
+    in
+    {
+      programs = lib.mkMerge [
+        {
+          git = {
+            enable = true;
+            lfs.enable = true;
+            signing = {
+              format = "ssh";
+              key = "~/.ssh/id_ed25519.pub";
+              signByDefault = true;
+            };
+          };
+        }
+        (mkCompat
+          {
+            difftastic.enable = true;
+            git = {
+              settings = {
+                user.name = "quasigod";
+                user.email = "quasigod-io@proton.me";
+              }
+              // git_settings;
+            };
+          }
+          {
+            git = {
+              difftastic.enable = true;
+              userName = "quasigod";
+              userEmail = "quasigod-io@proton.me";
+              extraConfig = git_settings;
+            };
+          }
+        )
+      ];
     };
-  };
 }
