8b56afecd2063c32da5d2a2f1f4e6f1466bd4d15
quasigod <quasigod-io@proton.me>
2025-11-10 15:46:29 -0500

    add udev rule for wlmouse mice webusb

--- modules/toplevel/udev.nix
+++ modules/toplevel/udev.nix
@@ -2,5 +2,6 @@
   # lowest DPI of vertical mouse is 1200 for some reason
   unify.modules.workstation.nixos.services.udev.extraRules = ''
     ENV{ID_VENDOR_ID}=="248a", ENV{ID_MODEL_ID}=="8589", ENV{LIBINPUT_CALIBRATION_MATRIX}="0.5 0 0 0 0.5 0 0 0 1"
+    SUBSYSTEM=="hidraw", ATTRS{idVendor}=="36a7", TAG+="uaccess"
   '';
 }
