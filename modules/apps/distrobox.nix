553743705b674410cfe0cd6d7b61c365612454fb
quasigod <quasigod-io@proton.me>
2025-11-21 21:55:52 -0500

    remove nushell config to fix distrobox
    it works now that i launch nushell from bash

--- modules/apps/distrobox.nix
+++ modules/apps/distrobox.nix
@@ -2,19 +2,5 @@
   den.aspects.apps._.distrobox.homeManager.programs = {
     distrobox.enable = true;
     distrobox.enableSystemdUnit = false;
-    nushell = {
-      extraConfig = ''
-        use std/util "path add"
-
-        if "DISTROBOX_ENTER_PATH" in $env {
-          path add "/usr/local/sbin"
-          path add "/usr/local/bin"
-          path add "/usr/sbin"
-          path add "/usr/bin"
-          path add "/sbin"
-          path add "/bin"
-        }
-      '';
-    };
   };
 }
--- modules/apps/nushell.nix
+++ modules/apps/nushell.nix
@@ -43,17 +43,6 @@
                     algorithm: "fuzzy"
                   }
                 }
-
-                # fix paths in distrobox
-                use std/util "path add"
-                if "DISTROBOX_ENTER_PATH" in $env {
-                  path add "/usr/local/sbin"
-                  path add "/usr/local/bin"
-                  path add "/usr/sbin"
-                  path add "/usr/bin"
-                  path add "/sbin"
-                  path add "/bin"
-                }
               '';
             };
             carapace = {
