009371b41cec439dccf29d2b4cd2bb5cbd2bdbfa
quasigod <quasigod-io@proton.me>
2025-11-21 21:55:52 -0500

    switch from starship jujutsu patch to config
    no more rust builds yippee

--- modules/apps/shell.nix
+++ modules/apps/shell.nix
@@ -16,12 +16,6 @@
         };
         starship = {
           enable = true;
-          package = pkgs.starship.overrideAttrs {
-            patches = pkgs.fetchurl {
-              url = "https://patch-diff.githubusercontent.com/raw/starship/starship/pull/6089.patch";
-              hash = "sha256-6hSVUvVRr8UHHrfgzn/JOooELxz8xvZQwyQY5KJLvPU=";
-            };
-          };
           settings = {
             format = "$all";
             character = {
@@ -29,13 +23,35 @@
               error_symbol = "[➜](maroon)";
             };
             shell.disabled = false;
-            # For jujutsu
+            jobs.disabled = true; # TODO atuin creates a job during the prompt closure and the symbol_threshold option is bugged
+            # Jujutsu - https://github.com/jj-vcs/jj/wiki/Starship
+            custom.jj = {
+              command = ''
+                jj log --revisions @ --limit 1 --ignore-working-copy --no-graph --color always  --template '
+                  separate(" ",
+                    bookmarks.map(|x| truncate_end(10, x.name(), "…")).join(" "),
+                    tags.map(|x| truncate_end(10, x.name(), "…")).join(" "),
+                    surround("\"", "\"", truncate_end(24, description.first_line(), "…")),
+                    if(conflict, "conflict"),
+                    if(divergent, "divergent"),
+                    if(hidden, "hidden"),
+                  )
+                '
+              '';
+              when = "jj --ignore-working-copy root";
+              symbol = "jj ";
+            };
+            custom.jjstate = {
+              command = ''
+                jj log -r@ -n1 --ignore-working-copy --no-graph -T "" --stat | tail -n1 | sd "(\d+) files? changed, (\d+) insertions?\(\+\), (\d+) deletions?\(-\)" ' ''\${1}m ''\${2}+ ''\${3}-' | sd " 0." ""
+              '';
+              when = "jj --ignore-working-copy root";
+            };
             git_branch.disabled = true;
             git_commit.disabled = true;
             git_state.disabled = true;
             git_metrics.disabled = true;
             git_status.disabled = true;
-            jobs.disabled = true; # TODO atuin creates a job during the prompt closure and the symbol_threshold option is bugged
           };
         };
         atuin = {
