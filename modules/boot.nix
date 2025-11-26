2123b609339f3d5f48229ec5f2754f11794e5cc4
quasigod <quasigod-io@proton.me>
2025-11-26 16:49:34 -0500

    force systemd boot false for lanzaboote

--- modules/boot.nix
+++ modules/boot.nix
@@ -1,10 +1,10 @@
-{ inputs, ... }:
+{ inputs, lib, ... }:
 {
   styx.boot.provides = {
     secure.nixos = {
       imports = [ inputs.lanzaboote.nixosModules.lanzaboote ];
       boot = {
-        loader.systemd-boot.enable = false;
+        loader.systemd-boot.enable = lib.mkForce false;
         lanzaboote = {
           enable = true;
           pkiBundle = "/var/lib/sbctl";
--- modules/power-mgmt.nix
+++ modules/power-mgmt.nix
@@ -2,6 +2,7 @@
   styx.power-mgmt.nixos.services = {
     tuned.enable = true;
     thermald.enable = true;
+    tlp.enable = false;
     power-profiles-daemon.enable = false;
   };
 }
