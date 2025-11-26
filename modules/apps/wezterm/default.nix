111b7c16d0967ae04c652ba5b9c60afec628ae02
quasigod <quasigod-io@proton.me>
2025-11-26 16:06:23 -0500

    patch wezterm to fix clipboard issues

--- modules/apps/wezterm/default.nix
+++ modules/apps/wezterm/default.nix
@@ -1,13 +1,35 @@
+{ withSystem, ... }:
 {
-  styx.apps._.wezterm = {
-    homeManager.programs.wezterm = {
-      enable = true;
-      extraConfig = builtins.readFile ./config.lua;
+  perSystem =
+    { pkgs, ... }:
+    {
+      packages.wezterm = pkgs.wezterm.overrideAttrs (
+        final: prev: {
+          patches = (prev.patches or [ ]) ++ [
+            (pkgs.fetchpatch {
+              url = "https://github.com/wezterm/wezterm/commit/3f062e0aa1924dc5666a57b0e7d065cc26a3b29b.patch";
+              hash = "sha256-o9nVO91R+eOMdtJM+8X2yHI0ejllbv4lC0aLW8ua4ok=";
+            })
+          ];
+        }
+      );
     };
+  styx.apps._.wezterm = {
+    homeManager =
+      { pkgs, ... }:
+      {
+        programs.wezterm = {
+          enable = true;
+          package = (withSystem pkgs.stdenv.hostPlatform.system (p: p.config.packages.wezterm));
+          extraConfig = builtins.readFile ./config.lua;
+        };
+      };
     nixos =
       { pkgs, ... }:
       {
-        environment.systemPackages = [ pkgs.wezterm ];
+        environment.systemPackages = [
+          (withSystem pkgs.stdenv.hostPlatform.system (p: p.config.packages.wezterm))
+        ];
         xdg.terminal-exec.settings.default = [ "org.wezfurlong.wezterm.desktop" ];
       };
   };
