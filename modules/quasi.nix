eafc76026bb2dec2373b4b87d96fcf3e09469f3d
quasigod <quasigod-io@proton.me>
2026-02-11 01:32:49 -0500

    add quasi ssh pub keys

--- modules/quasi.nix
+++ modules/quasi.nix
@@ -1,4 +1,4 @@
-{ den, __findFile, ... }:
+{ __findFile, ... }:
 {
   den.aspects.quasi = {
     includes = [
@@ -7,10 +7,16 @@
       <styx/nushell>
       <styx/shell>
     ];
-    nixos.users.users.quasi.extraGroups = [
-      "docker"
-      "wireshark"
-    ];
+    nixos.users.users.quasi = {
+      openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIezBKQfXf4ZHTKyCBavmlIXVFhJ7xd4MltEubJe9Kxe quasi@hades"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBQ8s7HeNm2rc4jxPdontGgXy3am1qSkXDbOgnT4Kz+J quasi@zagreus"
+      ];
+      extraGroups = [
+        "docker"
+        "wireshark"
+      ];
+    };
   };
   den.hosts.x86_64-linux.hades.users.quasi = { };
   den.hosts.x86_64-linux.zagreus.users.quasi = { };
